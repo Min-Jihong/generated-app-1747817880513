7270c6f4902a2a6f89ac165dad45cdca01a1cb3b
Min-Jihong <69140228+Min-Jihong@users.noreply.github.com>
2025-05-21 17:58:13 +0900

    Add src/app/(auth)/login/page.tsx

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import LoginForm from '../../../components/forms/login-form';
+
+export default function LoginPage() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <LoginForm />
+    </div>
+  );
+}
